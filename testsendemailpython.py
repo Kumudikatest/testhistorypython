04fa28b27c1d221e215a1ca99a8c5563216158df
Kumudikatest <kumudikarupasinghe+adrt@gmail.com>
2020-03-10 18:25:40 +0530

    tt

--- testsendemailpython.py
+++ testsendemailpython.py
@@ -3,14 +3,14 @@ import json
 ses = boto3.client("ses")
 
 def handler(event, context):
-    #print(event)
+    print(event)
 
     tabledetails = json.loads(json.dumps(event["Records"][0]["dynamodb"]))
     print(tabledetails)
-    
-    name = tabledetails.NewImage.Name.S;
-    email = tabledetails.NewImage.Email.S;
-    url = tabledetails.NewImage.URL.S;
+
+    name = tabledetails["NewImage"]["Name"]["S"];
+    email = tabledetails["NewImage"]["Email"]["S"];
+    url = tabledetails["NewImage"]["URL"]["S"];
     messagebody = 'Hi'+ ' ' + name + '! your shopping cart is waiting for you. Follow this link to return to your cart' + ' ' + url
 
     try:
